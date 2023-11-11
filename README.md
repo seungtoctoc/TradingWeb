e75436bade28bb2b13cc447bd31665917d8570ec
seungtoctoc <dltmxor31@naver.com>
2023-11-11 20:09:26 +0900

    refactor: 수직 공백 조정 및 들여쓰기 조정 수직 공백과 들여쓰기를 조정했습니다.

---
+++ README.md
@@ -0,0 +1,37 @@
+**물건 거래 웹사이트**
+flask, SQLAlchemy 활용<br><br>
+
+<p align="center">
+  <img src="https://github.com/seungtoctoc/TradingWeb/assets/102455571/53e0e8b2-4ec8-4456-84fd-9ad50474d217">
+</p>
+
+- 로그인, 회원가입 <br><br>
+
+<p align="center">
+  <img src="https://github.com/seungtoctoc/TradingWeb/assets/102455571/ac39d8d5-22b6-426c-bd0e-34427cfb0637">
+</p>
+
+- 판매 중인 물건 확인
+- 물건 업로드 (로그인 시)<br><br>
+
+<p align="center">
+  <img src="https://github.com/seungtoctoc/TradingWeb/assets/102455571/9485b93f-916b-4bbd-a9c4-28b4e610c4a6">
+</p>
+
+- 자세한 정보 확인
+- 판매자 팔로우 기능 (자신이 업로드한 물건이 아닌 경우)
+- 물건 판매 상태 전환 (자신이 업로드한 물건인 경우)<br><br>
+
+<p align="center">
+  <img src="https://github.com/seungtoctoc/TradingWeb/assets/102455571/16c07c7a-fc88-4a69-a650-701895bfd3cd">
+</p>
+
+- 판매 상태 전환이 적용된 모습
+- 팔로잉 중인 판매자의 판매 상품만 확인 (로그인 시)<br><br>
+
+<p align="center">
+  <img src="https://github.com/seungtoctoc/TradingWeb/assets/102455571/fa3836ce-4489-428c-9dd3-874e10dc1670">
+</p>
+
+- 자신의 판매 내역 확인 (로그인 시)
+- 물건 검색 기능
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import os
 from collections import UserDict
 from flask import Flask, render_template, request, url_for, redirect, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
-import os
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
@@ -10,8 +10,7 @@ app.config['SECRET_KEY'] = "secret"
 db = SQLAlchemy(app)
 
 # static 폴더로 경로 설정
-app.config['UPLOAD_FOLDER'] = '/Users/code_rosie/desktop/coding/py/static/'
-
+app.config['UPLOAD_FOLDER'] = '/Users/seungtoc/desktop/code/py/static/'
 
 class User_class(db.Model):
     id = db.Column(db.Integer, primary_key = True, unique = True, autoincrement = True)
@@ -30,8 +29,6 @@ class User_class(db.Model):
         
     def removeFollowing(self, id):
         self.u_following.remove(id)
-
-    
     
 class Product_class(db.Model):
     id = db.Column(db.Integer, primary_key = True, unique = True, autoincrement = True)
@@ -58,7 +55,6 @@ class Product_class(db.Model):
         self.p_info = info
         self.p_img = img
 
-
     def soldout(self):
         self.p_sale = "판매 완료"
         
@@ -70,11 +66,14 @@ class Product_class(db.Model):
 def main():
     if request.method == 'POST':
         return redirect(url_for('search', word = request.form['search']))
+    
     else:
         if 'username' in session:
             username = session['username']
             product_query = Product_class.query.all()
+            
             return render_template('main2.html', username = username, product_query = product_query)
+        
         else:
             product_query = Product_class.query.all()
             return render_template('main.html', product_query = product_query)
@@ -83,69 +82,69 @@ def main():
 def search(word):
     if request.method == 'POST':
         return redirect(url_for('search', word = request.form['search']))
+    
     else:    
         result = Product_class.query.filter(Product_class.p_name.like("%"+word+"%"))
+        
         if 'username' in session:
             return render_template('search2.html', username = session['username'], result=result)
+        
         else:
             return render_template('search.html', result=result)
-        
-        
         
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('main'))
 
-
-
 @app.route('/sign', methods=['GET', 'POST'])
 def sign():
     if request.method=='POST':
         if not request.form['id'] or not request.form['pw'] or not request.form['nm']:
            flash('Please enter all the fields', 'error')
+        
         else:
             user = User_class(request.form['id'], request.form['pw'], request.form['nm'])
             db.session.add(user)
             db.session.commit()
+            
             return redirect(url_for('login'))
     return render_template('sign.html')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method=='POST':
         if not request.form['id'] or not request.form['pw']:
             flash('Please enter all the fields', 'error')
+        
         else:
             inputId = request.form['id']
             inputPw = request.form['pw']
             data = User_class.query.filter_by(u_id = inputId, u_pw = inputPw).first()
+            
             if data is not None:
                 session['username'] = inputId
                 return redirect(url_for('main'))
-            else:
-                return render_template('login.html')
             
+            else:
+                return render_template('login.html') 
     return render_template('login.html')
-
 
 @app.route('/test')
 def test():
     return render_template('test.html', user_query = User_class.query.all(), product_query = Product_class.query.all())
-
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method=='POST':
         if not request.form['name'] or not request.form['price'] or not request.form['info'] or not request.files['file']:
             flash('Please enter all the fields', 'error')
+        
         else:
             name=request.form['name']
             f = request.files['file']
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
-            
-            
+             
             product = Product_class(name, request.form['price'], "판매중", session['username'], request.form['info'], secure_filename(f.filename))
             db.session.add(product)
             db.session.commit()
@@ -153,26 +152,28 @@ def upload():
             return redirect(url_for('main'))
     return render_template('upload.html', username = session['username'])
 
-
 @app.route('/detail/<p_name>', methods=['GET', 'POST'])
 def detail(p_name):
     if request.method == 'POST':
         return redirect(url_for('search', word = request.form['search']))
+    
     else:
         result = Product_class.query.filter_by(p_name = p_name).first()
+        
         if 'username' in session:
             username = session['username']
             result_user = User_class.query.filter_by(u_id = session['username']).first()
             return render_template('detail2.html', username = username, result = result, result_user = result_user)
+        
         else:
             return render_template('detail.html', result = result)
-        
         
 @app.route('/delete/<p_name>')
 def delete(p_name):
     result = Product_class.query.filter_by(p_name = p_name).first()
     db.session.delete(result)
     db.session.commit()
+    
     return redirect(url_for('test'))
 
 @app.route('/following')
@@ -181,12 +182,12 @@ def following():
     # followingList = result.u_following
     return render_template('following.html', username = session['username'], result=result, product_query = Product_class.query.all())
 
-
 @app.route('/follow/<id>')
 def follow(id):
     result = User_class.query.filter_by(u_id = session['username']).first()
     result.AddFollowing(id)
     db.session.commit()
+    
     return redirect(url_for('following'))
 
 @app.route('/unfollow/<id>')
@@ -194,6 +195,7 @@ def unfollow(id):
     result = User_class.query.filter_by(u_id = session['username']).first()
     result.removeFollowing(id)
     db.session.commit()
+    
     return redirect(url_for('following'))
 
 @app.route('/soldout/<p_name>')
@@ -201,6 +203,7 @@ def soldout(p_name):
     result = Product_class.query.filter_by(p_name=p_name).first()
     result.soldout()
     db.session.commit()
+    
     return redirect(url_for('detail', p_name = p_name))
 
 @app.route('/soldin/<p_name>')
@@ -208,6 +211,7 @@ def soldin(p_name):
     result = Product_class.query.filter_by(p_name=p_name).first()
     result.soldin()
     db.session.commit()
+    
     return redirect(url_for('detail', p_name = p_name))
 
 @app.route('/edit/<p_name>', methods=['GET', 'POST'])
@@ -216,6 +220,7 @@ def edit(p_name):
     if request.method=='POST':
         if not request.form['name'] or not request.form['price'] or not request.form['info'] or not request.files['file']:
             flash('Please enter all the fields', 'error')
+        
         else:
             name=request.form['name']
             f = request.files['file']
@@ -227,20 +232,15 @@ def edit(p_name):
             return redirect(url_for('main'))
     return render_template('edit.html', username = session['username'], result=result)
 
-
-
-
-
-
-
 @app.route('/selling', methods=['GET', 'POST'])
 def selling():
     if request.method == 'POST':
         return redirect(url_for('search', word = request.form['search']))
+    
     else:
         return render_template('selling.html', username = session['username'], product_query = Product_class.query.all())
 
-
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=5050)
